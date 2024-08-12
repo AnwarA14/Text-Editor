3400c835e3332396762273d159efd1e7bed36bae
AnwarA14 <anwarabdullah14104@gmail.com>
2024-08-12 19:27:18 +0100

    Change some colours

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,15 +1,6 @@
 package textEditor;
 
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JTextArea;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.filechooser.FileSystemView;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,6 +12,16 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.filechooser.FileSystemView;
 
 public final class TextEditor extends JFrame implements ActionListener {
     private static JTextArea area;
@@ -43,6 +44,9 @@ public final class TextEditor extends JFrame implements ActionListener {
         frame.add(area);
         frame.setSize(640,480);
         frame.setVisible(true);
+        
+        area.setBackground(Color.BLACK);
+        area.setForeground(Color.WHITE);
 
         JMenuBar menu_main = new JMenuBar();
 
